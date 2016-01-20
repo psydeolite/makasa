9135850bbc8d2fcf8a49585767617a8ac11eb6d6
masha <maryged@gmail.com>
2016-01-20 13:58:14 -0500

    stuff

--- chat.c
+++ chat.c
@@ -1,3 +1,17 @@
 #include "chat.h"
 
 //functions
+void parse_in(char input[]) {
+  if (!strcmp(input, "1")) {
+    //hit
+  } else if (!strcmp(input, "2")) {
+    //stand
+  } else if (!strcmp(input, "3")) {
+    //chat
+  } else {
+    printf("Invalid input innit bruv\n");
+  }
+}
+
+void hit() {
+  
--- chat.h
+++ chat.h
@@ -1,4 +1,13 @@
 //headers
+#include <stdio.h>
+#include <stdlib.h>
 
 //functions
+//(1)hit, (2)stand, (3)chat
+void parse_in();
 
+void hit();
+
+void stand();
+
+void chat();
