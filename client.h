8c2393d27dc2e1e40c290ae4066d734f4ae8e325
masha <maryged@gmail.com>
2016-01-24 17:05:55 -0500

    network framework

--- client.h
+++ client.h
@@ -1,3 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+typedef struct card {
+  int value;
+  int is_ace;
+  char* name;
+
+  struct card* next_card;
+} card;
 //headers
+//takes user's hand, new card dealt by dealer, boolean determining ace/one
+int hit (card* hand, card new, int is_one);
+
 
 //functions
